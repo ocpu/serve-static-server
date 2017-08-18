c822a4ac3e0200426b251ef726602a37b4b0c9ca
Martin Hövre <martin.hovre@opencubes.io>
2017-08-18 12:36:30 +0200

    New options and more reliable file types - Added package file-type - Changed default type from application/octet-stream to text/plain - New option to specify a custom 404 page - New option to only serve to the local computer

--- dev-server.js
+++ dev-server.js
@@ -9,14 +9,17 @@ if (getOption('-h, -help, --help', process.argv)) {
 
     Options:
 
-        -h, --help           Show this help message.
-        -v, --version     Show current version.
-        -p, --port <port>    The port to use when serving.
-
-    Directory can be omitted to use the current working directory.
+        -h, --help          Show this help message.
+        -v, --version       Show current version.
+        -p, --port <port>   The port to use when serving.
+        -f, --404 <file>    Show custom 404 page.
+        -l, --only-local    Force to only serve on local device.
 `)
     process.exit(0)
 }
+
+const file404 = getOption('-f, --404 <file>', process.argv)
+const onlyLocal = getOption('-l, --only-local', process.argv)
 
 // Get serve point and optional port
 const port = getOption('-p, --port <port>', process.argv) || 5000
@@ -24,32 +27,70 @@ const servePoint = process.argv.length === 3 ? require('path').resolve(process.a
 const path = require('path')
 const fs = require('fs')
 const extToMime = require('./extToMime.json')
+
 const app = (req, res) => {
     const url = require('url').parse(req.url)
     let fileToServe = servePoint + url.pathname.replace(/\//g, path.sep)
     if (fileToServe.endsWith(path.sep))
         fileToServe += 'index.html'
     try {
-        const { size } = fs.statSync(fileToServe)
-        res.setHeader('content-type', (extToMime[path.extname(fileToServe)] || extToMime['']) + '; charset=utf-8')
-        res.setHeader('content-length', size)
-        res.statusCode = 200
-        res.statusMessage = "OK"
-        fs.createReadStream(fileToServe).pipe(res)
+        serveFile(fileToServe, res)
     } catch (e) {
-        const content = new Buffer(`cannot ${req.method} ${req.url}`)
-        res.setHeader('content-type', 'text/plain; charset=utf-8')
-        res.setHeader('content-length', content.byteLength)
-        res.statusCode = 404
-        res.statusMessage = 'Not Found'
-        res.end(content)
+        if (file404) {
+            try {
+                serveFile(file404, res)
+            } catch (e) {
+                switch (e.code) {
+                    case'ENOENT':
+                        console.error(`404 file not found (${path.resolve(file404)})`)
+                        break
+                    default:
+                        console.error(e)
+                }
+                serveNotFound(req, res)
+            }
+        } else {
+            console.error(e)
+            serveNotFound(req, res)
+        }
     }
 }
-const addresses = discoverLocalAdresses()
-addresses.splice(0, 0, '127.0.0.1')
+
+function serveFile(file, res) {
+    const { size } = fs.statSync(file)
+    const smallBuffer = Buffer.alloc(20)
+    fs.readSync(fs.openSync(file, 'r'), smallBuffer, 0, smallBuffer.length, 0)
+    const fileType = require('file-type')(smallBuffer)
+    const mime = getMimeType(file)
+    if (mime)
+        res.setHeader('content-type', mime)
+    res.setHeader('content-length', size)
+    res.statusCode = 200
+    res.statusMessage = "OK"
+    fs.createReadStream(file).pipe(res)
+}
+
+function serveNotFound(req, res) {
+    const content = new Buffer(`Cannot ${req.method} ${req.url}`)
+    res.setHeader('content-type', 'text/plain; charset=utf-8')
+    res.setHeader('content-length', content.byteLength)
+    res.statusCode = 404
+    res.statusMessage = 'Not Found'
+    res.end(content)
+}
+
+function getMimeType(file) {
+    const smallBuffer = Buffer.alloc(20)
+    fs.readSync(fs.openSync(file, 'r'), smallBuffer, 0, smallBuffer.length, 0)
+    const fileType = require('file-type')(smallBuffer)
+    return fileType ? fileType.mime : extToMime[path.extname(file)] || ''
+}
+
+const addresses = ['127.0.0.1']
+if (!onlyLocal) Array.prototype.push.apply(addresses, discoverLocalAdresses())
 
 const logListen = callAfter(addresses.length, () => {
-    console.log(`Serving ${servePoint} on [${addresses.map(({ address, port }) => `http://${address}:${port}`).join(', ')}]`)
+    console.log(`Serving ${servePoint} on [ ${addresses.map(({ address, port }) => `http://${address}:${port}`).join(', ')} ]`)
 })
 
 addresses.forEach(function (address, index) {
