49e2f75fafdc7f6c914ad9d7dfe46f1ae711b634
ocpu <martin.hovre@opencubes.io>
2017-11-30 19:41:53 -0800

    Cleanup/refactoring to be a little more readable

--- dev-server.js
+++ dev-server.js
@@ -1,6 +1,24 @@
 #!/usr/bin/env node
 'use strict'
 
+// Native modules
+const { createGzip, createDeflate } = require('zlib')
+const { createHash } = require('crypto')
+const { promisify } = require('util')
+const path = require('path')
+const url = require('url')
+const fs = require('fs')
+
+// NPM modules
+const mime = require('mime')
+const fileType = require('file-type')
+const colors = require('colors')
+
+// Variables
+const readFile = promisify(fs.readFile)
+const exists = promisify(fs.exists)
+const read = promisify(fs.read)
+const open = promisify(fs.open)
 const program = require('commander')
   .version(require('./package.json').version, '-v, --version')
   .usage('[options] [directory]')
@@ -9,141 +27,124 @@ const program = require('commander')
   .option('-l, --only-local', 'force to only serve on local device')
   .parse(process.argv)
 
+// Program options
+const port = program.port
 const file404 = program[404]
 const onlyLocal = program.onlyLocal
-
-const mime = require('mime')
-const colors = require('colors')
-
-const { promisify } = require('util')
-const crypto = require('crypto')
-const path = require('path')
-const fs = require('fs')
-
-const readFile = promisify(fs.readFile)
-
-// Get serve point and optional port
-const port = program.port
 const servePoint = path.resolve(program.args[0] || '.')
 
-/**
- * 
- * @param {http.ClientRequest} req 
- * @param {*} res 
- */
+
 const app = async (req, res) => {
-  let fileToServe = servePoint + require('url').parse(req.url).pathname.replace(/\//g, path.sep)
+  // Get file to serve
+  let fileToServe = servePoint + url.parse(req.url).pathname.replace(/\//g, path.sep)
+  // If the path ends with / or \ append index.html
   if (fileToServe.endsWith(path.sep))
     fileToServe += 'index.html'
+
   try {
-    await serveFile(fileToServe, res, req)
+    await serveFile(fileToServe, res, req.headers)
   } catch (e) {
-    if (file404) {
-      try {
-        await serveFile(file404, res, req)
-      } catch (e) { resolveError(req, res, e, fileToServe) }
-    } else { resolveError(req, res, e, fileToServe) }
+    await serveNotFound(e, fileToServe, req, res)
   }
-  logRequest(req, res.statusCode)
+
+  const status = res.statusCode
+  const { method, url: path, headers } = req
+  const browser = colors.cyan('(' + getBrowserFromAgent(headers['user-agent']) + ')')
+  const requestMethod = colors.magenta(method)
+  const statusCode = colors[
+    status >= 400 ? 'red' :
+    status >= 300 ? 'blue' :
+    'green'
+  ](status)
+  console.log(`[${browser} ${requestMethod}/${statusCode}] ${colors.underline(path)}`)
 }
 
-const resolveError = (req, res, e, file) => {
-  if (e.code === 'ENOENT')
-    console.error(`Could not find resource ${path.resolve(file)}`)
-  else console.error(e)
-  serveNotFound(req, res)
-  logRequest(req, 404)
-}
-
-const logRequest = (req, status) => console.log(
-  `[${colors.cyan('('+getBrowserFromAgent(req.headers['user-agent'])+')')} ` +
-  `${colors.magenta(req.method)}/${colors[status >= 400 ? 'red' : status >= 300 ? 'blue' : 'green'](status)}] ${colors.underline(req.url)}`
-)
-
-/**
- * 
- * @param {string} file 
- * @param {ServerResponse} res 
- */
-async function serveFile(file, res, req) {
+async function serveFile(file, res, headers) {
+  // Test browser cache
   const etag = await getETag(file)
-  if (etag === req.headers['if-none-match']) {
+  if (etag === headers['if-none-match']) {
     res.statusCode = 304
     res.end()
     return
   }
-  const { size } = fs.statSync(file)
-  const encodingHeader = req.headers['accept-encoding']
-  const encoding =
-    ~encodingHeader.indexOf('gzip') ? 'gzip' :
-    ~encodingHeader.indexOf('deflate') ? 'deflate' :
-    'identity'
-  const resContent =
-    ~encodingHeader.indexOf('gzip') ? require('zlib').createGzip() :
-    ~encodingHeader.indexOf('deflate') ? require('zlib').createDeflate() :
-    res
+
+  // Set content encoding
+  const encodingHeader = headers['accept-encoding']
+  const [encoding, stream] =
+    ~encodingHeader.indexOf('gzip') ? ['gzip', createGzip] :
+    ~encodingHeader.indexOf('deflate') ? ['deflate', createDeflate] :
+    ['identity', res]
+
+  if (stream !== res) stream.pipe(res)
+  if (encoding === 'identity') res.setHeader('Content-Length', fs.statSync(file).size)
+  res.setHeader('Content-Encoding', encoding)
+
+  // Set mime type
+  const mimeType = await getMimeType(file)
+  if (mimeType) res.setHeader('Content-Type', mimeType)
   
-  if (resContent !== res) resContent.pipe(res)
-  const mimeType = getMimeType(file)
-  if (mimeType)
-    res.setHeader('Content-Type', mimeType)
-  if (encoding === 'identity')
-    res.setHeader('Content-Length', size)
+  // Caching
   res.setHeader('ETag', etag)
   res.setHeader('Cache-Control', 'public, max-age=31536000')
-  res.setHeader('Content-Encoding', encoding)
+
   res.setHeader('Vary', 'Accept-Encoding')
-  res.statusCode = 200
-  fs.createReadStream(file).pipe(resContent)
+  res.statusCode = res.statusCode || 200
+  fs.createReadStream(file).pipe(stream)
 }
 
-function serveNotFound(req, res) {
-  const content = new Buffer(`Cannot ${req.method} ${req.url}`)
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-  res.setHeader('Content-Length', content.byteLength)
-  // res.statusCode = 404
-  res.statusMessage = 'Not Found'
-  res.end(content)
+async function serveNotFound(e, file, req, res) {
+  res.statusCode = 404
+  if (!file404 && await exists(file404)) {
+    const content = new Buffer(`Cannot ${req.method} ${req.url}`)
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    res.setHeader('Content-Length', content.byteLength)
+    res.setHeader('Content-Encoding', 'identity')
+    res.end(content)
+  } else await serveFile(file404, res, req.headers)
+  
+  // Log error to console
+  if (e.code === 'ENOENT')
+    console.error(`Could not find resource ${path.resolve(file)}`)
+  else console.error(e)
 }
 
-async function getETag(file) {
-  return crypto.createHash('md5').update(await readFile(file)).digest('base64')
-}
+const getETag = async file =>
+  createHash('md5').update(await readFile(file)).digest('base64')
 
-function getMimeType(file) {
+async function getMimeType(file) {
   // First pass
   const mimeType = mime.getType(file)
   if (mimeType) return mimeType
 
   // Second pass
   const smallBuffer = Buffer.alloc(20)
-  fs.readSync(fs.openSync(file, 'r'), smallBuffer, 0, smallBuffer.length, 0)
-  const fileType = require('file-type')(smallBuffer)
-  if (fileType) return fileType.mime
+  const fd = await open(file, 'r')
+  await read(fd, smallBuffer, 0, smallBuffer.length, 0)
+  fs.close(fd)
+  const type = fileType(smallBuffer)
+  if (type) return type.mime
 
   // Default
   return 'text/plain'
 }
 
+// Get addresses to serve
 const addresses = ['127.0.0.1']
-if (!onlyLocal) Array.prototype.push.apply(addresses, discoverLocalAdresses())
+if (!onlyLocal) addresses.concat(discoverLocalAdresses())
 
-const logListen = callAfter(addresses.length, () => {
+const logListen = callAfter(addresses.length, () =>
   console.log(`Serving ${servePoint} on [ ${addresses.map(({ address, port }) => `http://${address}:${port}`).join(', ')} ]\n`)
-})
+)
 
-/**
- * 
- * @param {string} agent 
- */
 const getBrowserFromAgent = agent => 
   ~agent.indexOf('Chrome') ? 'Chrome' :
   ~agent.indexOf('Safari') ? 'Safari' :
   ~agent.indexOf('Firefox') ? 'Firefox' :
-  ~agent.indexOf('MSIE') ? 'Internet Explorer' :
   ~agent.indexOf('Edge') ? 'Egde' :
+  ~agent.indexOf('MSIE') ? 'Internet Explorer' :
   'Undefined'
 
+// For every address listen to the avaliable port
 addresses.forEach(function (address, index) {
   getPort(address, port, port => {
     require('http').createServer(app).listen(port, address).once('listening', function () {
@@ -174,26 +175,18 @@ function getPort(address, port, cb) {
   }
 }
 
+function* networkInterfaces() {
+  const interfaces = require('os').networkInterfaces()
+  for (const name in interfaces)
+    yield [ name, interfaces[name] ]
+}
+
 function discoverLocalAdresses() {
-  const os = require('os');
-  const interfaces = os.networkInterfaces()
   const addresses = []
-  for (const k in interfaces) {
-    for (const k2 in interfaces[k]) {
-      const address = interfaces[k][k2]
-      if (address.family === 'IPv4' && !address.internal) {
-        addresses.push(address.address)
-      }
-    }
-  }
+  for (const [, iface] of networkInterfaces()) for (const { address, family, internal } of iface)
+    if (family === 'IPv4' && !internal) addresses.push(address)
   return addresses
 }
 
-function callAfter(times, cb) {
-  let calls = 0
-  return function () {
-    calls++
-    if (calls >= times)
-      cb()
-  }
-}
+const callAfter = (times, cb, calls = 0) => () =>
+  ++calls >= times && cb()
